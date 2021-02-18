05c77b56faf0f39a1ddbf5d1fa3148e64b7d3e73
EvanSnyder2023 <816547@mystma.org>
2021-02-18 16:16:27 +0000

    calender try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def calander(m):
+
+        for i in range(0, len(month)):
+            if m == month[i]:
+                return i + 1
+
+        return m + " is not a month"
+
+month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+print(calander("donut"))
